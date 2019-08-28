e2f51f69cf5310d3ec304ccdcdb8007436604c7b
Brandon M <bmooremsu@gmail.com>
2019-08-28 19:13:12 -0400

    more words

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import pandas as pd
 
 tabtitle = 'DC Housing'
 myheading='Analysis of housing prices in Washington DC'
-neighborhood='Washington Highlands'
+neighborhood='Congress Heights'
 color1='#00FF00'
 color2='#1B03B1'
 sourceurl = 'https://www.kaggle.com/christophercorrea/dc-residential-properties/'
